8765bf6625d044b274d0c77ee296031309dc26d6
Alexandre Ferreira <alexandrejfneto@gmail.com>
2023-10-24 16:34:56 -0300

    FindByID implementation

--- backend/src/main/java/com/alejfneto/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/alejfneto/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.alejfneto.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,13 @@ public class CategoryService {
 	public List<CategoryDTO> findAll(){
 		List <Category> list = repository.findAll();
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional (readOnly=true)
+	public CategoryDTO findByID(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		CategoryDTO catDTO = new CategoryDTO(obj.get());
+		return catDTO;
 	}
 
 }
