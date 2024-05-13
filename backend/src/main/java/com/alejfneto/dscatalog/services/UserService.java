590a6c4b173ede6333fc1886c49f5bea8d9eb658
Alexandre Ferreira <alexandrejfneto@gmail.com>
2024-05-13 15:06:36 -0300

    OAuth2 Check List Implemented

--- backend/src/main/java/com/alejfneto/dscatalog/services/UserService.java
+++ backend/src/main/java/com/alejfneto/dscatalog/services/UserService.java
@@ -10,7 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -39,7 +39,7 @@ public class UserService implements UserDetailsService {
 	private RoleRepository roleRepository;
 	
 	@Autowired
-	private BCryptPasswordEncoder passwordEncoder;
+	private PasswordEncoder passwordEncoder;
 
 	@Transactional(readOnly = true)
 	public Page<UserDTO> findAllPaged(Pageable pageable){
